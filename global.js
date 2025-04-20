4f091461c76af4b4ba192302715ce0f5f15f54a6
randonDsc <pochen8787@gmail.com>
2025-04-19 18:25:13 -0700

    lab 3 3.1

--- global.js
+++ global.js
@@ -4,14 +4,46 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$('nav a');
+// const navLinks = $$('nav a');
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
 
 
-if (currentLink) {
-    // or if (currentLink !== undefined)
-    currentLink.classList.add('current');
+// if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+//   } step 2
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/randonDsc', title: 'GitHub' },
+  ];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+? "/"                  // Local server
+: "/website/";         // GitHub Pages repo name
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+  
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+      }
+
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
+
+
+
+
+
+
