27763ddf30d833a7f2b0b1adf4670bc076b01e51
randonDsc <pochen8787@gmail.com>
2025-05-02 21:36:48 -0700

    step2 complete

--- projects/projects.js
+++ projects/projects.js
@@ -21,7 +21,7 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 //   endAngle: 2 * Math.PI,
 // });
 
-//d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
 
 // let data = [1, 2];
@@ -43,8 +43,16 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 // let arcs = arcData.map((d) => arcGenerator(d));
 
-let data = [1, 2, 3, 4, 5, 5];;
-let sliceGenerator = d3.pie();
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -55,7 +63,15 @@ arcs.forEach((arc, idx) => {
   d3.select('svg')
   .append('path')
   .attr('d', arc)
-  .attr('fill', colors(idx))
+  .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+.append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
 
 
